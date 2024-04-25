fad04877b32c1a20c261dd0262ddbf362c62daf8
promise <ugobrigh31@gmail.com>
2024-04-25 02:46:09 +0100

    added firebase almost done with the shopping page

---
+++ e-commerce/design inspirations/app.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAq-8oCRmbOQ9wYeZxj1N63FhLy95iQo60",
+  authDomain: "unnbazaar-15e21.firebaseapp.com",
+  projectId: "unnbazaar-15e21",
+  storageBucket: "unnbazaar-15e21.appspot.com",
+  messagingSenderId: "467972538509",
+  appId: "1:467972538509:web:0a4ee689dba8c4cf8422c4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
