7cd0bf9ba193f50916b82e53d91809c0220e1b17
promise <ugobrigh31@gmail.com>
2024-04-26 03:49:07 +0100

    adding javascript json to easily parse new products instead of manually adding products in html, this is why I like react

--- e-commerce/design inspirations/app.js
+++ e-commerce/design inspirations/app.js
@@ -16,4 +16,3 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
---
+++ e-commerce/design inspirations/product.js
@@ -0,0 +1,15 @@
+const products = 
+    [
+        {
+            "name":"Kings Vegetable Oil",
+            "price":"N39,000",
+            "description":"Use am fry akara,fry plantain,use am cook rice",
+            "url":"e-commerce\Assets\productsImage\kingsOil.png"
+        },
+        {
+            "name":"Kings Vegetable Oil",
+            "price":"N39,000",
+            "description":"Use am fry akara,fry plantain,use am cook rice",
+            "url":"e-commerce\Assets\productsImage\kingsOil.png"
+        }
+    ]
